9236bfe5a4d780fe0febdc980965a24d962ed84f
TamjidAhmed10 <tamjidahammad10@gmail.com>
2023-01-25 08:24:02 +0600

    learning ex

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json());
 
 app.get("/", (req, res) => {
     console.log(req.url);
@@ -11,11 +12,24 @@ app.get("/abc", (req, res) => {
     res.json("hello world")
 })
 
+
 app.get("/search", (req, res) => {
     console.log(req.url);
     console.log(req.query);
     res.json("hello world")
 })
+
+app.get("/headers", (req, res) => {
+    console.log(req.headers)
+    res.json("hello world")
+})
+
+app.use(express.urlencoded({ extended: true }));
+app.post('/', (req, res) => {
+    console.log(req.body); // logs the request body as an object
+    res.send("done ")
+});
+
 
 const port = 9000 
 
